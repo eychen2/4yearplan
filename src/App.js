1f2e2908ac14ac2d32403838f88638c181d7fa38
eychen2 <echen963@gmail.com>
2021-10-25 09:38:02 -0400

    made starting code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactFlow from 'react-flow-renderer';
+const elements = [
+  {
+    id: '1',
+    type: 'input', // input node
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 },
+  },
+  // default node
+  {
+    id: '2',
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: '3',
+    type: 'output', // output node
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+];
+const onNodeMouseEnter = (event,node)=>{
+
 }
-
-export default App;
+const onNodeMouseLeave = (event, node) => {
+}
+export default () => (
+  <div style={{ height: 720 }}>
+    <ReactFlow elements={elements}  onNodeMouseEnter={onNodeMouseEnter}/>
+  </div>
+);
