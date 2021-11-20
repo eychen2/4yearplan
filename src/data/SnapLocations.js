9ad3006941e205785a45fb4d9d4d2f7a894acf90
Thomas Mulligan <thomas.mulligan@ufl.edu>
2021-11-19 19:10:40 -0500

    commit

--- src/data/SnapLocations.js
+++ src/data/SnapLocations.js
@@ -1,6 +1,12 @@
 const SnapLocations=[
-    {x:300, y:300},
-    {x:600, y:600}
+    {
+        x:300,
+        y:300
+    },
+    {
+        x:600,
+        y:600
+    }
 ]
 
 module.exports=SnapLocations;
